c3e6a176e620dc3c84323a32f0f427da0d056d87
Virendra Shiral <166993591+virendrashiral@users.noreply.github.com>
2025-10-03 18:34:33 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-   base: "/",
+   // The base path should be the name of your GitHub repository.
+   // This is crucial for GitHub Pages deployments in a sub-directory.
+   base: "/google-jam/", 
   plugins: [react()],
   
 })
